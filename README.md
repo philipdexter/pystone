0abb8e916285af3f01e85a7dc2c13aca873c6373
Philip Dexter <philip.dexter@gmail.com>
2015-09-07 17:08:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Pystone
+
+This is a quickly put together hearthstone (deck) tracker made for linux.
+
+To use this tracker you must setup hearthstone to track your games:
+follow
+[these](https://www.reddit.com/r/hearthstone/comments/268fkk/simple_hearthstone_logging_see_your_complete_play)
+instructions.
+
+It is very fragile right now and I wouldn't be surprised if it didn't
+work on other setups.
+
+## Requirements
+
+Just
+[click](http://click.pocoo.org/5/).
+
+## Usage
+
+Running the program:
+
+```bash
+python track.py path/to/hearthstone/output_log.txt command
+```
+
+For example my path is
+`~/.PlayOnLinux/wineprefix/hearthstone/drive_c/Program\
+Files/Hearthstone/Hearthstone_Data/output_log.txt`.
+
+Currently the only commands are `track` and `clear`.  `track` runs the
+deck tracker and `clear` clears old log data.  More tips below.
+
+Before every new game (before you queue) **and before the first time
+you run it** you need to run the `clear` command (fix this in future).
+
+Sometimes it seems to get stuck somehow in the middle of a game. If
+this happens just Ctrl-C and restart it in `track` mode (fix this in
+future).
+
+## Future Plans
+
+I have plans.
---
+++ track.py
@@ -0,0 +1,103 @@
+import sys
+import re
+import click
+import copy
+
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+my_deck = {
+    'Humility': 1,
+    'Equality': 2,
+    'Doomsayer': 1,
+    'Sunfury Protector': 2,
+    'Wild Pyromancer': 2,
+    'Aldor Peacekeeper': 2,
+    'Big Game Hunter': 1,
+    'Truesilver Champion': 2,
+    'Consecration': 2,
+    'Defender of Argus': 1,
+    'Piloted Shredder': 2,
+    'Holy Wrath': 2,
+    'Antique Healbot': 1,
+    'Sludge Belcher': 2,
+    'Emperor Thaurissan': 1,
+    'Sylvanas Windrunner': 1,
+    'Dr. Boom': 1,
+    'Lay on Hands': 1,
+    'Tirion Fordring': 1,
+    'Molten Giant': 2,
+}
+
+my_current_deck = copy.copy(my_deck)
+
+def determine_color(card):
+    if my_deck[card] == my_current_deck[card]:
+        return OKGREEN
+    elif my_deck[card] == 2 and my_current_deck[card] == 1:
+        return OKBLUE
+    else:
+        return FAIL
+
+def draw(c):
+    global my_current_deck
+    if c in my_current_deck:
+        my_current_deck[c] -= 1
+
+def put_back(c):
+    global my_current_deck
+    if c in my_current_deck:
+        my_current_deck[c] += 1
+    else:
+        my_current_deck[c] = 1
+
+def loop(f):
+    while True:
+        line = f.readline().strip()
+        if not line:
+            continue
+        r = re.compile(r'^\[Zone\].*TRANSITIONING.*name=(.*)id.*zone=(PLAY|HAND|DECK).*player=(.*)]')
+        m = r.match(line)
+        if m:
+            card = m.group(1).strip()
+            action = m.group(2).strip()
+            player = m.group(3).strip()
+            yield (player, card, action)
+
+@click.group()
+@click.argument('logfile', type=click.Path(exists=True))
+@click.pass_context
+def cli(ctx, logfile):
+    ctx.obj['logfile'] = logfile
+
+@click.command(help='Start the main deck tracker')
+@click.pass_context
+def track(ctx):
+    with open(ctx.obj['logfile'], 'r') as f:
+        print(HEADER+'--------\nleft in deck'+ENDC+':\n{}'.format('\n'.join(sorted([(determine_color(card)+'{}: {}'+ENDC).format(card, count) for card,count in sorted(my_current_deck.items(), key=lambda x: x[0])]))))
+        for p, c, a in loop(f):
+            if p == '1':
+                if a == 'HAND':
+                    draw(c)
+                elif a == 'DECK':
+                    put_back(c)
+            print(HEADER+'--------\nleft in deck'+ENDC+':\n{}'.format('\n'.join(sorted([(determine_color(card)+'{}: {}'+ENDC).format(card, count) for card,count in sorted(my_current_deck.items(), key=lambda x: x[0])]))))
+
+@click.command(help='Clear the hearthstone log file')
+@click.pass_context
+def clear(ctx):
+    with open(ctx.obj['logfile'], 'w') as f:
+        f.write('')
+    print('log file cleared')
+
+cli.add_command(track)
+cli.add_command(clear)
+
+if __name__ == '__main__':
+    cli(obj={})
